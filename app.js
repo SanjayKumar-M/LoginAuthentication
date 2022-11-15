bf5b358be16456502544a71cc35ec02a8928c346
SanjayKumar-M <sanjaymoad786@gmail.com>
2022-11-15 15:00:20 +0530

    sss

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
-app.get('/',(req, res)=>{res.send("<h1>Hello Web3</h1><h2>Sanjay</h2?")})
+
+app.get('/',(req, res)=>{res.send("<h1>Hello Web3</h1>")})
 
 app.listen(8080,()=>{console.log("Listening to the port...")})
